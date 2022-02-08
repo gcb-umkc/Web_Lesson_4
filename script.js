9d8e9071a4030abacd4409c4b6c7064ca771aed3
gcb-umkc <gcbz9k@mail.umkc.edu>
2022-02-07 20:33:45 -0600

    Finished request JSON handler code

--- script.js
+++ script.js
@@ -6,22 +6,31 @@ function getGithubInfo(user) {
     let request = new XMLHttpRequest();
     request.open("GET", URL);
     request.send();
-    console.log(request.responseText);
     return request;
 }
 
+//Struct for storing the user info.
 function userInfo(name, avatar, id, url) {
     this.name = name;
     this.avatar = avatar;
     this.ID = id;
     this.URL = url
+    return this;
 }
 
 function showUser(user) {
     //2. set the contents of the h2 and the two div elements in the div '#profile' with the user content
+    let name = "";
+    if (user["name"] == ""){
+        name = user["login"];
+    }
+    else{
+        name = user["name"];
+    }
 
-
-
+    //Creates a new user struct to store relevant request information
+    let newUser = userInfo(name, user["avatar_url"], user["id"], user["url"]);
+    return newUser;
 }
 
 function noSuchUser(username) {
