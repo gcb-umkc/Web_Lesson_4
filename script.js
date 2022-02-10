cd9fffb855806fe624568003f8a9da56ea5e0444
gcb-umkc <gcbz9k@mail.umkc.edu>
2022-02-09 19:35:23 -0600

    Added initial code for displaying information

--- script.js
+++ script.js
@@ -9,8 +9,6 @@ function getGithubInfo(user) {
     return request;
 }
 
-
-
 //Struct for storing the user info.
 function userInfo(user) {
     if (user["name"] == null){
@@ -21,18 +19,22 @@ function userInfo(user) {
     }
 
     this.avatar = user["avatar_url"];
-    this.ID = user["id"];
-    this.URL = user["html_url"];
+    this.id = user["id"];
+    this.hurl = user["html_url"];
     return this;
 }
 
 function showUser(user) {
     //Creates a new user struct to store relevant request information
     let newUser = userInfo(user);
-  
+
     //2. set the contents of the h2 and the two div elements in the div '#profile' with the user content
-    $('#userid').text(user.id)
-    $('#name').text(user.name)
+    $('#id').text(newUser.id);
+    $('#name').text(newUser.name);
+    $('#avatar').text(newUser.avatar);
+    console.log(newUser.hurl.innerHTML);
+    $('#html_url').text(newUser.hurl.toString());
+    console.log(newUser.hurl);
 }
 
 function noSuchUser(username) {
@@ -42,20 +44,21 @@ function noSuchUser(username) {
 $(document).ready(function () {
     $(document).on('keypress', '#username', function (e) {
         //check if enter (i.e return) key is pressed
+        let response;
         if (e.which == 13) {
             //get what the user enters
-            username = $(this).val();
-            console.log(username)
             let username = $(this).val();
+            console.log(username)
             //reset the text typed in the input
             $(this).val("");
             //get the user's information and store the response
-            response = getGithubInfo(username);
+            let response = getGithubInfo(username);
             //if the response is successful show the user's details
             if (response.status == 200) {
-                console.log(response.status)
-                showUser(JSON.parse(response.responseText));
-                console.log(showUser(JSON.parse(response.responseText)))
+                console.log(response.status);
+                let newUser = userInfo(JSON.parse(response.responseText));
+                console.log(newUser.ID);
+                showUser(newUser);
                 //else display suitable message
             } else {
                 noSuchUser(username);
